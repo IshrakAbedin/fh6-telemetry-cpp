8b84a6f171cc432d4a49615fcc26f59bdc656be0
Ishrak Abedin <ishrak.abedin.a1@gmail.com>
2026-06-21 00:10:57 +0600

    Added commandline arg for port number

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,24 @@
 #include <cstdio>
 #include <fmt/base.h>
 #include <stdexcept>
+#include <string>
 #include <thread>
 
 #include "telemetry_packet.hpp"
 #include "telemetry_server.hpp"
 #include "util.hpp"
 
-constexpr auto PORT = 7777;
+constexpr auto DEFAULT_PORT = 7777;
 
-int main()
+int main(int argc, char* argv[])
 {
     using namespace std::chrono_literals;
     try
     {
+        TelemetryServer::port_type PORT =
+            argc < 2 ? DEFAULT_PORT : std::stoi(argv[1]);
+        fmt::println("Starting server in port: {} ...", PORT);
+
         asio::io_context io_context;
         TelemetryServer server{io_context, PORT};
         auto server_thread =
