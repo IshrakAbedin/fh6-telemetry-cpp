71307c8583cdab359b4e3a358212148512fcbdd9
Ishrak Abedin <ishrak.abedin.a1@gmail.com>
2026-06-23 20:30:11 +0600

    Updated window title

--- src/ui/ui_config.hpp
+++ src/ui/ui_config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr auto WINDOW_TITLE = "ImPlot Demo Test";
+constexpr auto WINDOW_TITLE = "Forza Horizon 6 Telemetry";
 constexpr auto WINDOW_WIDTH = 1280;
 constexpr auto WINDOW_HEIGHT = 720;
 constexpr auto DEFAULT_FONT = "c:/Windows/Fonts/segoeui.ttf"; // Placeholder for now
